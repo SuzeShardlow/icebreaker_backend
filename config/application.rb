b8761459c1aa06c961a2c3bbf83c0d8acf783c12
SuzeShardlow <suze.shardlow1@gmail.com>
2017-09-04 14:05:04 +0100

    need to make a proxy for meetup api

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -28,9 +29,18 @@ module WDIProject4Api
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     origins '*'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
+    # end
+
     config.api_only = true
 
     config.eager_load_paths << Rails.root.join('lib')
+
 
   end
 end
